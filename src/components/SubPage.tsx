4440d6c8c62fa0aad5dbb93432c7dc8d610e7889
matheusrodrigues1 <matech00@outlook.com>
2024-10-18 16:33:06 -0300

    homepage and subpage

--- src/components/SubPage.tsx
+++ src/components/SubPage.tsx
@@ -11,16 +11,16 @@ const SubPage = () => {
       <h1 className="font-extrabold text-center text-3xl pr-9 pl-9 text-white mb-20">
         A GOLDCARD É PARA VOCE QUE:
       </h1>
-      <div className="flex flex-col items-center justify-center w-[30rem] h-[37rem] rounded-lg bg-black">
+      <div className="flex flex-col items-center justify-center w-[25rem] h-[37rem] rounded-lg bg-black">
         <Image
           src={Sub01}
-          className="h-[14rem] w-[20rem] rounded-lg"
+          className="h-[12rem] w-[20rem] rounded-lg"
           alt="img"
         />
-        <span className="text-2xl text-zinc-400 font-extrabold text-center pr-10 pl-10 mt-5">
+        <span className="text-xl text-zinc-400 font-extrabold text-center pr-10 pl-10 mt-5">
           ESTÁ CANSADO DE PAGAR CARO EM PACOTES DE TV?
         </span>
-        <p className="text-zinc-300 text-2xl font-normal text-center pr-10 pl-10 mt-6">
+        <p className="text-zinc-300 text-2xl font-normal text-center pr-10 pl-10 mt-6 leading-tight">
           Chega de tomar sustos com aumentos que Você não esperava nas
           mensalidades da sua fatura de TV fechada!{" "}
           <span className="font-bold text-white">
@@ -28,16 +28,16 @@ const SubPage = () => {
           </span>
         </p>
       </div>
-      <div className="flex flex-col items-center justify-center w-[30rem] h-[37rem] rounded-lg bg-black mt-5">
+      <div className="flex flex-col items-center justify-center w-[25rem] h-[37rem] rounded-lg bg-black mt-5">
         <Image
           src={Sub02}
-          className="h-[14rem] w-[20rem] rounded-lg"
+          className="h-[12rem] w-[20rem] rounded-lg"
           alt="img"
         />
         <span className="text-2xl text-zinc-400 font-extrabold text-center pr-10 pl-10 mt-5">
           Não tem paciência para atendentes de Operadoras
         </span>
-        <p className="text-zinc-300 text-2xl font-normal text-center pr-10 pl-10 mt-6">
+        <p className="text-zinc-300 text-2xl font-normal text-center pr-10 pl-10 mt-6 leading-tight">
           Chega de perder a parte mais importante do que você está assistindo.
           Não se preocupe com quedas e instabilidades de sinal!{" "}
           <span className="font-bold text-white">
@@ -45,16 +45,16 @@ const SubPage = () => {
           </span>
         </p>
       </div>
-      <div className="flex flex-col items-center justify-center w-[30rem] h-[37rem] rounded-lg bg-black mt-5">
+      <div className="flex flex-col items-center justify-center w-[25rem] h-[37rem] rounded-lg bg-black mt-5">
         <Image
           src={Sub03}
-          className="h-[14rem] w-[20rem] rounded-lg"
+          className="h-[12rem] w-[20rem] rounded-lg"
           alt="img"
         />
         <span className="text-2xl text-zinc-400 font-extrabold text-center pr-10 pl-10 mt-5">
           Quer ter estabilidade de sinal
         </span>
-        <p className="text-zinc-300 text-2xl font-normal text-center pr-10 pl-10 mt-6">
+        <p className="text-zinc-300 text-2xl font-normal text-center pr-10 pl-10 mt-6 leading-tight">
           Chega de perder a parte mais importante do que você está assistindo.
           Não se preocupe com quedas e instabilidades de sinal!{" "}
           <span className="font-bold text-white">
@@ -62,16 +62,16 @@ const SubPage = () => {
           </span>
         </p>
       </div>
-      <div className="flex flex-col items-center justify-center w-[30rem] h-[37rem] rounded-lg bg-black mt-5">
+      <div className="flex flex-col items-center justify-center w-[25rem] h-[37rem] rounded-lg bg-black mt-5">
         <Image
           src={Sub04}
-          className="h-[14rem] w-[20rem] rounded-lg"
+          className="h-[12rem] w-[20rem] rounded-lg"
           alt="img"
         />
         <span className="text-2xl text-zinc-400 font-extrabold text-center pr-10 pl-10 mt-5">
           Está cansado de pagar caro em pacotes de TV
         </span>
-        <p className="text-zinc-300 text-2xl font-normal text-center pr-10 pl-10 mt-6">
+        <p className="text-zinc-300 text-2xl font-normal text-center pr-8 pl-8 mt-6 leading-tight">
           Já comprou um produto que pouco tempo depois parou de funcionar?{" "}
           <span className="font-bold text-white">
             Nós te damos suporte exclusivo e atualizamos semanalmente os filmes
